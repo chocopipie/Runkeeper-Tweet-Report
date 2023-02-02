a0780279873e4282e184e2130290dda146485f9d
Van Pham <thyvan1412@gmail.com>
2023-02-02 03:21:22 -0800

    added search functionality on descriptions.js

--- js/descriptions.js
+++ js/descriptions.js
@@ -14,19 +14,36 @@ function parseTweets(runkeeper_tweets) {
 
 	//TODO: Filter to just the written tweets
 	written_tweet_array = tweet_array.filter(tweet => tweet.written)
-	//console.log(written_tweet_array)
 
-
-	written_tweet_array.forEach(tweet => {
-		console.log(tweet.link)
-	})
-	let table = $('.table')
-	for (let i = 0; i < written_tweet_array.length; i++) {
-		table.append(written_tweet_array[i].getHTMLTableRow(i+1))
-	}
+	// let tableBody = $('#tweetTable')
+	// for (let i = 0; i < written_tweet_array.length; i++) {
+	//  	tableBody.append(written_tweet_array[i].getHTMLTableRow(i+1))
+	// }
 }	
 
 function addEventHandlerForSearch() {
+	let tableBody = $('#tweetTable')
+	$('#textFilter').on('keyup', function(event) {
+		tableBody.empty()
+		let searchText = $(this).val()
+		
+		$('#searchText').text(searchText)
+
+		let filteredArray = []
+		if (searchText !== "") {
+			filteredArray = written_tweet_array.filter(tweet => {
+				if ((tweet.text).toLowerCase().includes(searchText.toLowerCase()))
+					return tweet;
+			})
+		}
+
+		$('#searchCount').text(filteredArray.length);
+
+		for (let i = 0; i < filteredArray.length; i++) {
+			tableBody.append(filteredArray[i].getHTMLTableRow(i+1))
+		}
+	})
+		
 	//TODO: Search the written tweets as text is entered into the search box, and add them to the table
 }
 
